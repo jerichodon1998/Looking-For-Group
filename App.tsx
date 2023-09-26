2bab70d7e6613d40359526db57ebc8d075549a21
markjericho <jerichodon1998@gmail.com>
2023-09-26 20:57:17 +0800

    setting up redux toolkit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import "react-native-gesture-handler";
 import AppNavigation from "./src/navigation/AppNavigation";
+import { Provider } from "react-redux";
+import { store } from "./src/redux/app/store";
 
 export default function App() {
-	return <AppNavigation />;
+	return (
+		<Provider store={store}>
+			<AppNavigation />
+		</Provider>
+	);
 }
