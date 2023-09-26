7e0e01ee0b35d710df82fa96dd2c5389e7062215
markjericho <jerichodon1998@gmail.com>
2023-09-26 21:51:12 +0800

    setup react-dot-env and enable redux devtools conditionally

--- src/redux/app/store.ts
+++ src/redux/app/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "../features/counter/counterSlice";
-
+import { ENV } from "@env";
 export const store = configureStore({
 	reducer: {
 		counter: counterSlice,
 	},
+	devTools: ENV !== "production",
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
