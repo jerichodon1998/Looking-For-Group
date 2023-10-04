1d275f6284fff34880872245480fba7d7080b735
markjericho <jerichodon1998@gmail.com>
2023-10-04 10:16:52 +0800

    messagesScreen created

---
+++ src/screens/MessagesScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const MessagesScreen = (): JSX.Element => {
+	return (
+		<View>
+			<Text>Messages</Text>
+		</View>
+	);
+};
+
+export default MessagesScreen;
