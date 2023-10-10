98dbd494b172fd30f6ddf65c7b7d80face09a21e
markjericho <jerichodon1998@gmail.com>
2023-10-10 09:25:51 +0800

    FeedItem navigation added

--- src/navigation/authenticatedStack/AuthStackNavigator.tsx
+++ src/navigation/authenticatedStack/AuthStackNavigator.tsx
@@ -1,11 +1,17 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import AuthTopTabNavigator from "./AuthTopTabNavigator";
+import FeedItemScreen from "../../screens/FeedItemScreen";
 const Stack = createStackNavigator();
 
 const StackNavigator = (): JSX.Element => {
 	return (
 		<Stack.Navigator screenOptions={{ headerShown: false }}>
 			<Stack.Screen name="Home" component={AuthTopTabNavigator} />
+			<Stack.Screen
+				name="FeedItem"
+				component={FeedItemScreen}
+				options={{ presentation: "modal" }}
+			/>
 		</Stack.Navigator>
 	);
 };
