a1f5f9fcb550d227e85c0bccaf5ad3138b45eccb
markjericho <jerichodon1998@gmail.com>
2023-10-10 09:27:12 +0800

    render feed

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,28 @@
-import { View, Text } from "react-native";
+import { View, ListRenderItem } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { TweetInterface, tweets } from "../dummyData/dummyTweets";
+import { useCallback } from "react";
+import { FlatList } from "react-native-gesture-handler";
+import FeedItemComponent from "../components/FeedItemComponent";
 const HomeScreen = (): JSX.Element => {
+	const renderItem: ListRenderItem<TweetInterface> = useCallback(
+		({ item }) => {
+			return <FeedItemComponent tweet={item} />;
+		},
+		[tweets]
+	);
+
 	return (
 		<SafeAreaView>
 			<View>
-				<Text>Home</Text>
+				<FlatList
+					data={tweets}
+					removeClippedSubviews={true}
+					initialNumToRender={10}
+					renderItem={renderItem}
+					windowSize={5}
+					keyExtractor={(item) => item.id}
+				/>
 			</View>
 		</SafeAreaView>
 	);
