d5684118d4d0b3448c1f5abe92f29849f04dd9dd
markjericho <jerichodon1998@gmail.com>
2023-10-12 12:00:21 +0800

    profile edit and modal

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -5,19 +5,26 @@ import {
 	Image,
 	Pressable,
 	Modal,
-	Button,
+	TextInput,
 } from "react-native";
-import { useAppSelector } from "../../redux/app/hooks";
+import { useAppDispatch, useAppSelector } from "../../redux/app/hooks";
 import { Ionicons } from "@expo/vector-icons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {
+	resetProfile,
+	userUpdateProfile,
+} from "../../redux/features/profile/profileSlice";
+import ErrorMessageComponent from "../../components/ErrorMessageComponent";
 
-interface UserProfileUpdateInterface {
+export interface UserProfileUpdateInterface {
 	displayName: string | undefined | null;
 	photoURL: string | undefined | null;
 }
 
 const ProfileScreen = (): JSX.Element => {
 	const authStore = useAppSelector((state) => state.auth);
+	const profileStore = useAppSelector((state) => state.profile);
+	const dispatch = useAppDispatch();
 	const [modalVisible, setModalVisible] = useState(false);
 	const [userProfileUpdate, setUserProfileUpdate] =
 		useState<UserProfileUpdateInterface>({
@@ -25,12 +32,62 @@ const ProfileScreen = (): JSX.Element => {
 			photoURL: authStore.userData?.photoURL,
 		});
 
+	const openEditModal = () => setModalVisible(true);
+	const closeEditModal = () => {
+		setUserProfileUpdate({
+			displayName: authStore.userData?.displayName,
+			photoURL: authStore.userData?.photoURL,
+		});
+		setModalVisible(false);
+	};
+	const onSaveEditModal = () => {
+		dispatch(resetProfile());
+		dispatch(userUpdateProfile(userProfileUpdate));
+	};
+
+	// NOTE: Refactor this as a reusable component
 	const myModal = (): JSX.Element => {
 		return (
-			<Modal visible={modalVisible} transparent={true}>
-				<View style={{ backgroundColor: "grey", height: 100 }}>
-					<Text>Modal</Text>
-					<Button title="close" onPress={() => setModalVisible(false)} />
+			<Modal
+				statusBarTranslucent={true}
+				visible={modalVisible}
+				transparent={true}
+				animationType="slide"
+				onRequestClose={closeEditModal}
+			>
+				<View style={styles.modalStyle}>
+					<View style={styles.modalContentStyle}>
+						<View style={styles.modalHeaderContainer}>
+							<Text style={styles.modalHeaderText}>Edit Profile</Text>
+						</View>
+						<View style={styles.modalInputContainer}>
+							<Text>Name:</Text>
+							<TextInput
+								style={styles.modalTextInput}
+								onChangeText={(text) =>
+									setUserProfileUpdate((prevState) => {
+										return { ...prevState, displayName: text };
+									})
+								}
+								value={userProfileUpdate.displayName || ""}
+							/>
+						</View>
+						{profileStore.error ? (
+							<ErrorMessageComponent
+								errorMessage={profileStore.error?.message}
+							/>
+						) : null}
+						<View style={styles.modalBtnContainer}>
+							<Pressable style={styles.modalBtn} onPress={onSaveEditModal}>
+								<Ionicons name="checkmark-circle" size={18} color="green" />
+								<Text>Save</Text>
+							</Pressable>
+							<Pressable style={styles.modalBtn} onPress={closeEditModal}>
+								<Ionicons name="close-circle" size={18} color="red" />
+								<Text>Cancel</Text>
+							</Pressable>
+						</View>
+					</View>
 				</View>
 			</Modal>
 		);
@@ -69,9 +126,9 @@ const ProfileScreen = (): JSX.Element => {
 		}
 	};
 
-	const openEditModal = () => {
-		setModalVisible(true);
-	};
+	useEffect(() => {
+		dispatch(resetProfile());
+	}, [dispatch]);
 
 	return (
 		<View style={styles.profileContainer}>
@@ -116,7 +173,7 @@ const styles = StyleSheet.create({
 	},
 	headerDetailsContainer: {
 		flexDirection: "row",
-		alignItems: "flex-end",
+		alignItems: "center",
 	},
 	headerTextStyle: {
 		fontWeight: "bold",
@@ -153,6 +210,56 @@ const styles = StyleSheet.create({
 	},
 	bioTextStyle: {
 		fontSize: 16,
+	},
+	modalStyle: {
+		flex: 1,
+		backgroundColor: "rgba(128,128,128, 0.5)",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	modalContentStyle: {
+		backgroundColor: "white",
+		borderColor: "grey",
+		borderWidth: 0.8,
+		borderRadius: 10,
+		paddingHorizontal: 15,
+		paddingVertical: 8,
+		width: "70%",
+
+		// shadow effect
+		shadowColor: "#000",
+		shadowOffset: { width: -2, height: 4 },
+		shadowOpacity: 1,
+		shadowRadius: 5,
+		elevation: 5,
+	},
+	modalHeaderContainer: {
+		alignItems: "center",
+	},
+	modalHeaderText: {
+		fontSize: 24,
+		fontWeight: "bold",
+	},
+	modalInputContainer: {},
+	modalTextInput: {
+		borderColor: "grey",
+		borderRadius: 5,
+		borderWidth: 1,
+		marginVertical: 10,
+	},
+	modalBtnContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+	modalBtn: {
+		flexDirection: "row",
+		alignItems: "center",
+		backgroundColor: "white",
+		paddingHorizontal: 10,
+		paddingVertical: 5,
+		borderColor: "grey",
+		borderWidth: 1,
+		width: 80,
 	},
 });
 
